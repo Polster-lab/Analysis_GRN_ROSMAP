1d032752d221f6caeb7dfbec986522aca5829ba4
Danish <danish2562022@gmail.com>
2024-06-30 15:12:26 +0000

    Added seperate GRN analysis

---
+++ Gene_Regulatory_Network_ROSMAP/code/community_detection_Tf/differential_expression_analysis/.ipynb_checkpoints/differential_expression_analyst-checkpoint.py
@@ -0,0 +1,44 @@
+import os
+import pickle as pkl
+
+import pandas as pd
+
+from pydeseq2.dds import DeseqDataSet
+from pydeseq2.default_inference import DefaultInference
+from pydeseq2.ds import DeseqStats
+
+
+
+def differential_expression_analysis(output_dir, count_matrix, meta_data):
+
+    inference = DefaultInference(n_cpus=8)
+    dds = DeseqDataSet(
+        counts=count_matrix,
+        metadata=meta_data,
+        design_factors="condition",
+        refit_cooks=True,
+        inference=inference,
+    )
+
+    dds.deseq2()
+
+    print(dds)
+    print(dds.varm["dispersions"])
+    print(dds.varm["LFC"])
+
+    stat_res = DeseqStats(dds, inference=inference)
+    print(stat_res.summary())
+    stat_res.results_df.to_csv(os.path.join(output_dir, "results.csv"))
+
+
+
+count_matrix = pd.read_csv('count_matrix_diff_analysis.csv',index_col=0)
+meta_data = pd.read_csv('meta_data_diff_analysis.csv',index_col=0)
+meta_data = meta_data[meta_data.condition.isin(['B','C'])]
+count_matrix = count_matrix.T
+count_matrix = count_matrix[count_matrix.index.isin(meta_data.index)]
+print(count_matrix)
+print(meta_data)
+
+
+differential_expression_analysis(output_dir='./different_expression_output/B_C', count_matrix=count_matrix, meta_data=meta_data)
